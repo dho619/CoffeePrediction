c8d1a428a8510658476c786afed997e375910112
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-11-11 00:38:33 -0300

    Adicionando rota do feedback

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -1,30 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { Text, TouchableOpacity, View, TextInput } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { Text, TouchableOpacity, View, TextInput, Alert } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
+import api from '../../services/api';
+import { Context } from '../../context/contextAuth';
+import { isOnline } from '../../services/Network';
 import Header from '../../components/Header';
 import { pickerStyle, styles } from './styles';
 
-
-
 export default function Feedback({ navigation }) {
+    const [online, setOnline] = useState(false);
     const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
+    const [subject, setSubject] = useState('-1');
     const [subjects, setSubjects] = useState([]);
     const [feedback, setFeedback] = useState('');
 
+    const { token } = useContext(Context);
+
     useEffect(() => {
-        setSubjects([
-            { label: "Relatar Bug", value: "Relatar Bug" },
-            { label: "Sugestão de Melhoria", value: "Sugestão de Melhoria" },
-            { label: "Outros", value: "Outros" }
-        ]);
+        const loadInfo = async () => {
+            const situation = await isOnline();
+            setOnline(situation);
+            setSubjects([
+                { label: "Relatar Bug", value: "Relatar Bug" },
+                { label: "Sugestão de Melhoria", value: "Sugestão de Melhoria" },
+                { label: "Outros", value: "Outros" }
+            ]);
+        }
+        let mounted = true;
+        loadInfo();
+        return () => mounted = false;
     }, []);
 
-    const submitFeedback = () => {
+    const clearFields = () => {
+        setEmail('');
+        setSubject('');
+        setFeedback('');
+    };
 
+
+    const submitFeedback = async () => {
+        if (!online) {
+            Alert.alert(
+                "Aviso",
+                'Desculpe, não encontramos conexão com a internet!',
+                [
+                    { text: "OK" }
+                ],
+                { cancelable: false }
+            );
+        }
+        if (subject == -1) {
+            Alert.alert(
+                "Aviso",
+                'Nenhum assunto selecionado',
+                [
+                    { text: "OK" }
+                ],
+                { cancelable: false }
+            );
+            return '';
+        }
+
+        if (feedback === '') {
+            Alert.alert(
+                "Aviso",
+                'Nenhuma mensagem a ser enviada',
+                [
+                    { text: "OK" }
+                ],
+                { cancelable: false }
+            );
+            return '';
+        }
+
+
+        const feedbackInfo = {
+            subject,
+            message: feedback,
+            returnEmail: email
+        }
+        try {
+            const response = await api.post('sendFeedback', feedbackInfo, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            Alert.alert(
+                "Sucesso",
+                'Muito obrigado pelo feedback, ele é muito importante para nós!!!',
+                [
+                    { text: "OK" }
+                ],
+                { cancelable: false }
+            );
+
+            clearFields();
+        } catch (err) {
+            Alert.alert(
+                "Erro",
+                'Erro ao enviar feedback, tente novamente mais tarde!',
+                [
+                    { text: "OK" }
+                ],
+                { cancelable: false }
+            );
+        }
     }
 
     return (
